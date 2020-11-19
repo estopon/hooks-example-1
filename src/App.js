4483cea23b2da89ae26cdbbf9346e48f14880a08
estopon <estopon@gmail.com>
2020-11-19 07:51:57 +0100

    State example

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,7 +21,18 @@ function App() {
         >
           Learn React
         </a>
+        {/* State example */}
+        <p>You clicked {count} times</p>
+        <button onClick={() => setCount(count + 1)}>
+          Click me
+        </button>
       </header>
+      <div>
+        <p>You clicked {count} times</p>
+        <button onClick={() => setCount(count + 1)}>
+          Click me
+        </button>
+      </div>
     </div>
   );
 }
